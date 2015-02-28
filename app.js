acdeb2b146626fd5637d8f56a80dbe342ad4ddd6
Chris Crowell <crowelch@mail.uc.edu>
2015-02-28 08:53:20 -0600

    add request js

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var request = require('request');
 
 var app = express();
 
@@ -62,45 +63,6 @@ app.use(function(err, req, res, next) {
 
 
 module.exports = app;
-
-app.get('/auth/facebook', function(req, res) {
-
-  // we don't have a code yet
-  // so we'll redirect to the oauth dialog
-  if (!req.query.code) {
-    var authUrl = graph.getOauthUrl({
-        "client_id":     conf.client_id
-      , "redirect_uri":  conf.redirect_uri
-      , "scope":         conf.scope
-    });
-
-    if (!req.query.error) { //checks whether a user denied the app facebook login/permissions
-      res.redirect(authUrl);
-    } else {  //req.query.error == 'access_denied'
-      res.send('access denied');
-    }
-    return;
-  }
-
-  // code is set
-  // we'll send that and get the access token
-  graph.authorize({
-      "client_id":      conf.client_id
-    , "redirect_uri":   conf.redirect_uri
-    , "client_secret":  conf.client_secret
-    , "code":           req.query.code
-  }, function (err, facebookRes) {
-    res.redirect('/UserHasLoggedIn');
-  });
-
-
-});
-
-
-// user gets sent here after being authorized
-app.get('/UserHasLoggedIn', function(req, res) {
-  res.render("index", { title: "Logged In" });
-});
 
 
 var server = app.listen(3000, function () {
