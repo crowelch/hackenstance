c144accc2f84698d2af78a76a3a1360e06096e8b
Chris Crowell <crowelch@mail.uc.edu>
2015-02-28 01:17:08 -0600

    add index.html

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ var app = express();
 
 var Firebase = require('firebase');
 var firebase = new Firebase('https://hackenstance.firebaseio.com/');
+var graph = require('fbgraph');
 
 
 // view engine setup
@@ -26,7 +27,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
+//app.use('/', routes);
+app.use('/', express.static(__dirname + '/public/index.html'));
 app.use('/users', users);
 
 // catch 404 and forward to error handler
