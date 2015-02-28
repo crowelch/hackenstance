013f2d243e6ffff398355d43b13b9c5b6a6f1d94
Chris Crowell <crowelch@mail.uc.edu>
2015-02-28 16:09:00 -0600

    Add api call to /facebook, takes ?id=xxxx where xxxx is the userid

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ var config = require('./config/config.json');
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
@@ -29,6 +30,14 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.get('/facebook', function(req, res) {
+    firebase.child('users/' + req.query.id + '/facebook/accessToken').on('value', function(data) {
+        //console.log();
+        //res.type('text/json');
+        //res.send('hi');
+      res.send(data.val()); 
+    });
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
